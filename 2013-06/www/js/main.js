126197af7c886b8b3b174af7711fc780d946b4b0
Maarten Billemont <lhunath@lyndir.com>
2014-05-14 23:00:37 -0400

    mediaqueries.css -> responsive.css, tweaks and info updates.

--- 2013-06/www/js/main.js
+++ 2013-06/www/js/main.js
@@ -1,5 +1,7 @@
 $(function(){
     if( ! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
         $.stellar();
+    else
+        $("header, section").css('background-attachment', 'fixed');
 });
 
