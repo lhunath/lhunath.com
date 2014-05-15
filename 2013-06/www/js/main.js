4a2ac4b51bb8e288f1fce7fca6457a2e18ac786f
Maarten Billemont <lhunath@lyndir.com>
2014-05-14 23:26:14 -0400

    responsive width fixes.

--- 2013-06/www/js/main.js
+++ 2013-06/www/js/main.js
@@ -1,7 +1,5 @@
 $(function(){
     if( ! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
         $.stellar();
-    else
-        $("header, section").css('background-attachment', 'fixed');
 });
 
